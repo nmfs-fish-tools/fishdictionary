2e19ac985f0943e96907844cbaebd2026dc73e69
Christine Stawitz <christine.stawitz@noaa.gov>
2022-07-21 11:07:29 -0700

    change wellPanel to mainPanel

--- inst/Shiny/server.r
+++ inst/Shiny/server.r
@@ -20,6 +20,7 @@ server <- function(input, output, session) {
 
   output$documentation <- renderUI({
     rdfile <- paste0(input$topic, ".Rd")
+    print(rdfile)
     req(rdfile %in% names(rd_database()))
     tools::Rd2HTML(rd_database()[[rdfile]], tmp,
     package = "stockassessmentdictionary")
