1e96c1e9cadc283edbae571dce9472e9b292f55c
Christine Stawitz <christine.stawitz@noaa.gov>
2022-07-21 11:41:25 -0700

    move .css into /Shiny dir

--- inst/Shiny/ui.r
+++ inst/Shiny/ui.r
@@ -1,6 +1,6 @@
 require(shiny)
 require(stockassessmentdictionary)
-ui <- fluidPage(theme = "../www/nmfs-styles.css",
+ui <- fluidPage(theme = "nmfs-styles.css",
   uiOutput("choose_topic"),
   wellPanel(uiOutput("documentation"))
 )
