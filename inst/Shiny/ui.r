a3f0c6c24e132841f01de2b9e7209a913f642a81
Christine Stawitz <christine.stawitz@noaa.gov>
2022-07-21 12:54:52 -0700

    fix erroneaous )

--- inst/Shiny/ui.r
+++ inst/Shiny/ui.r
@@ -2,6 +2,6 @@ require(shiny)
 require(stockassessmentdictionary)
 ui <- fluidPage(theme = "nmfs-styles.css",
   uiOutput("choose_topic"),
-  wellPanel(uiOutput("documentation"))
-  wellPanel(uiOutput("foodweb")) 
-))
+  wellPanel(uiOutput("documentation")),
+  wellPanel(uiOutput("foodweb"))
+)
