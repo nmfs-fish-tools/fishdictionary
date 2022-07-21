1e1a1ba1f0ac66febf5db28a6d4641877b62ed10
Christine Stawitz <christine.stawitz@noaa.gov>
2022-07-21 13:16:46 -0700

    stop assigning to obj

--- inst/Shiny/server.r
+++ inst/Shiny/server.r
@@ -27,7 +27,7 @@ server <- function(input, output, session) {
     htmltools::includeHTML(tmp)})
 
   output$foodweb <- renderPlot({
-    plotout <- mvbutils::foodweb(where = "package:stockassessmentdictionary",
+    mvbutils::foodweb(where = "package:stockassessmentdictionary",
      prune = input$Term)
   })
 }
