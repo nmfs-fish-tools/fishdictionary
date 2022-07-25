66717b3e2d5974e15a50a861c4644a9bf17a0ded
Bai Li <bai.li@noaa.gov>
2022-07-25 10:10:18 -0400

    use nmfs-style css

--- inst/Shiny/ui.r
+++ inst/Shiny/ui.r
@@ -1,13 +1,8 @@
 require(shiny)
 require(stockassessmentdictionary)
 ui <- fluidPage(
-  #theme = "nmfs-styles.css",
-  includeCSS("extra.css"),
-  # includeCSS("../../www/extra.css"),
-  # tags$head(
-  #   tags$style(HTML("
-  #     @import url('https://nmfs-fish-tools.github.io/nmfspalette/extra.css');"))
-  # ),
+  # includeCSS("extra.css"),
+  includeCSS("nmfs-styles.css"),
   uiOutput("choose_topic"),
   wellPanel(uiOutput("documentation"))
 )
