52cec81dca8b55cf7e5291de98a2759c54138c4b
Bai Li <bai.li@noaa.gov>
2022-07-25 09:56:11 -0400

    include css

--- inst/Shiny/ui.r
+++ inst/Shiny/ui.r
@@ -1,6 +1,13 @@
 require(shiny)
 require(stockassessmentdictionary)
-ui <- fluidPage(theme = "nmfs-styles.css",
+ui <- fluidPage(
+  #theme = "nmfs-styles.css",
+  includeCSS("../../extra.css"),
+  # includeCSS("../../www/extra.css"),
+  # tags$head(
+  #   tags$style(HTML("
+  #     @import url('https://nmfs-fish-tools.github.io/nmfspalette/extra.css');"))
+  # ),
   uiOutput("choose_topic"),
   wellPanel(uiOutput("documentation"))
 )
