b9d6387bd16abc0da69dbc542fe9eb51fe730996
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2022-09-08 10:10:12 -0700

    Adds projection
    Close #31
    Close #20

---
+++ R/Projection.R
@@ -0,0 +1,15 @@
+#' Projection
+#'
+#' A probabilistic statement about future events based on what might happen.
+#'
+#' @format
+#' \describe{
+#' \item{Examples}{short-term projection}
+#' \item{Rationale}{Future weather is a forecast because it does not require
+#' an if statement, whereas future spawning biomass is a projection because
+#' it depends on an assumption about future catches.}
+#' \item{Alternatives}{forecast, prediction}
+#' \item{Range of possible values}{}
+#' \item{Units}{}
+#' }
+Projection <- NULL
