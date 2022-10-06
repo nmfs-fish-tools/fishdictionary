63f42ff012adfecbc745133c26ed0b8245af61ee
Bai Li <bai.li@noaa.gov>
2022-10-06 15:28:43 -0400

    test: update testthat data path

--- tests/testthat/test-validate-Rd.R
+++ tests/testthat/test-validate-Rd.R
@@ -1,5 +1,13 @@
 test_that("validate_Rd validators work as expected", {
 
+  pkg_path <- find.package("fishdictionary")
+  
+  if (!file.exists(file.path(pkg_path, "inst"))) {
+    validate_Rd_path <- file.path(pkg_path, "extdata", "testthat_data")
+  } else {
+    validate_Rd_path <- file.path(pkg_path, "inst", "extdata", "testthat_data")
+  }
+  
   Rd_file <- file.path(validate_Rd_path, "validate_Rd_no_description.Rd")
   expect_error(validate_Rd(Rd_file), 
                regexp = "Description is not provided. This should be capitalized and not end in a period.")
@@ -19,10 +27,6 @@ test_that("validate_Rd validators work as expected", {
   Rd_file <- file.path(validate_Rd_path, "validate_Rd_no_range_of_possible_values.Rd")
   expect_error(validate_Rd(Rd_file),
                regexp = "Range of possible values is not provided. Use NA if there is no input for range of possible values.")
-  
-  Rd_file <- file.path(validate_Rd_path, "validate_Rd_no_units.Rd")
-  expect_error(validate_Rd(Rd_file),
-               regexp = "Units are not provided. Use NA if there is no input for units.")
   
   Rd_file <- file.path(validate_Rd_path, "validate_Rd_no_units.Rd")
   expect_error(validate_Rd(Rd_file),
