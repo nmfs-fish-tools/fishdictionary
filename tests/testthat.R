5aa77d31a242486cfb985c933af70f9cc1f9fc35
Bai Li <bai.li@noaa.gov>
2022-10-06 15:17:38 -0400

    change testthat data path

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,4 @@ library(testthat)
 library(fishdictionary)
 library(tools) # For test-validate-Rd.R
 
-# Use "extdata" in "inst" because its loaded with R packages
-validate_Rd_path <- system.file(file.path("extdata", "testthat_data"), package = "fishdictionary")
-
 test_check("fishdictionary")
