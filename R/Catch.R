b883cb7034118be2ec9228079c12effbae0c0889
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2023-10-17 07:19:40 -0700

    fix(Catch.R): use Synonym or Similar Terms

--- R/Catch.R
+++ R/Catch.R
@@ -8,7 +8,7 @@
 #' \item{Rationale}{Landings and catch are sometimes thought to be
 #' interchangeable but they are not given that catch can also include
 #' bycatch or unwanted catch.}
-#' \item{Alternatives}{total mortality, harvest, total removals}
+#' \item{Synonym or Similar Terms}{total mortality, harvest, total removals}
 #' \item{Range of possible values}{0--Inf}
 #' \item{Units}{mt, numbers}
 #' }
